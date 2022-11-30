36e05d8ddc772508400e5c5ca59b574706dbfe12
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-30 16:39:23 +0000

    Request data successfully

--- src/containers/ChartBox.js
+++ src/containers/ChartBox.js
@@ -1,35 +1,60 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import './ChartBox.css';
 
 const apiEndpoint = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json";
 
+const makeSong = (entry, index) => {
+    return {
+        _index: index,
+        id: entry["id"],
+        name: entry["im:name"],
+        title: entry["title"],
+        artist: entry["im:artist"],
+        category: entry["category"]
+    }
+}
+
+const getSongs = async () => {
+    const res = await fetch(apiEndpoint);
+
+    if (res.status !== 200)
+        throw new Error('Did not get 200 response');
+
+    const data = await res.json();
+
+    console.log("data", Object.keys(data));
+    /* only key is 'feed' */
+    console.log("data.feed", Object.keys(data.feed));
+    /* keys are 'author', 'entry', 'updated', 'rights', 'title', 'icon', 'link', 'id' */
+    console.log("data.feed.entry[0]", Object.keys(data.feed.entry[0]));
+    /* keys are 'im:name', 'im:image', 'im:collection', 'im:price', 'im:contentType', 'rights', 'title', 'link', 'id', 'im:artist', 'category', 'im:releaseDate' */
+
+    const songs = data.feed.entry.map(makeSong);
+
+    return songs;
+};
+
 const ChartBox = () => {
-    // const [releases, setReleases] = useState(ukFilms);
-    // let [region, setRegion] = useState("uk");
+    const [songs, setSongs] = useState([]);
 
-    // if (!(region in staticData)) {
-    //     setRegion("uk");
-    //     region = "uk";
-    // }
+    useEffect(() => {
+        getSongs().then(setSongs)
+    }, []);
 
-    // const regionObj = staticData[region];
-    // const releases = regionObj.films;
-    // const regionNames = mapEntries(staticData, ([region, data]) => [region, data.name]);
+    const songItems = songs.map(song => {
+        return <li key={song._index}>
+            {/* { Object.keys(song).map((k) => (<div>k: <code>{JSON.stringify(song[k]})</code></div>)) } */}
+            { Object.keys(song).map((k) => (<div>{k}: <code>{JSON.stringify(song[k])}</code></div>)) }
+        </li>
+    });
 
-    // return <aside className="ReleaseBox">
-    //     <ReleasesTitle currentRegion={region} regionNames={regionNames} onRegionChange={setRegion} />
-    //     <hr />
-    //     {/*<button onClick={ (e) => { if (region === "uk") { setRegion("fr") } else { setRegion("uk")}} }>TEST REGION</button>
-    //     <hr />*/}
-
-    //     <ReleaseList releases={releases} />
-
-    //     <hr />
-    //     <MoreReleases href={viewMoreLink(regionObj.region)}/>
-    // </aside>;
-
-    return <p>ChartBox</p>
+    return <>
+        <h2>ChartBox</h2>
+        <ul>
+            { songItems }
+        </ul>
+    </>;
 };
 
 export default ChartBox;
